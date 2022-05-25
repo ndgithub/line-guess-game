340854f658be74ac0c602c8e422f7ac616a68096
ndesai <nickydesai.mail@gmail.com>
2022-05-24 21:15:41 -0500

    Adds random lines logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Lines } from './Lines';
 
 export const Game = (props) => {
-    return (
-        <><input type="number"></input>
-            <button onClick={props.onSubmit}>Guess</button>
-            < Lines />
-        </>)
-}
+  return (
+    <div id='game-box'>
+      <input type='number'></input>
+      <button onClick={props.onSubmit}>Guess</button>
+      <Lines />
+    </div>
+  );
+};
--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,17 +1,50 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
 export const Lines = (props) => {
-    const ref = useRef(null);
+  const ref = useRef();
 
-    useEffect(() => {
-        useEffect(() => {
-            console.log('height: ', ref.current.clientHeight);
-            console.log('width: ', ref.current.clientWidth);
-        }, []);
+  const [lines, setLines] = useState({});
 
-    });
+  const [points1, setPoints1] = useState('0,0 0,0');
+  const [points2, setPoints2] = useState('0,0 0,0');
 
-    return (
-        <div>Lines</div>
-    )
-}
+  let line1;
+  let line2;
+
+  const makeLine = (width, height) => {
+    return {
+      x1: Math.floor(Math.random() * width),
+      y1: Math.floor(Math.random() * height),
+      x2: Math.floor(Math.random() * width),
+      y2: Math.floor(Math.random() * height),
+    };
+  };
+
+  useEffect(() => {
+    line1 = makeLine(ref.current.clientWidth, ref.current.clientHeight);
+    line2 = makeLine(ref.current.clientWidth, ref.current.clientHeight);
+    setLines({ line1, line2 });
+    console.log(toPoints(line1));
+    setPoints1(toPoints(line1));
+    setPoints2(toPoints(line2));
+  }, []);
+  const getLength = (lineObj) => {};
+  const toPoints = (lineObj) =>
+    `${lineObj.x1},${lineObj.y1} ${lineObj.x2},${lineObj.y2}`;
+
+  return (
+    <div id='line-box' ref={ref}>
+      <svg height='600' width='500'>
+        <polyline
+          points={points1}
+          style={{ fill: 'none', stroke: 'black', strokeWidth: 3 }}
+        />
+        <polyline
+          points={points2}
+          style={{ fill: 'none', stroke: 'black', strokeWidth: 3 }}
+        />
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    </div>
+  );
+};
